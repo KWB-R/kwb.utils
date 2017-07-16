350c656ccb6a24b740b6f873c83ed8a6205e037f
hsonne <hauke.sonnenberg@gmx.de>
2017-07-16 08:04:24 +0200

    add test for unmerge()

---
+++ tests/testthat/test_unmerge.R
@@ -0,0 +1,37 @@
+# checkResult ------------------------------------------------------------------
+checkResult <- function(x, xy)
+{
+  standardise <- function(x) kwb.utils::fullySorted(x[, sort(names(x))])
+  
+  x <- standardise(x)
+  y <- standardise(merge(xy$x, xy$y))
+  
+  isIdentical <- identical(x, y)
+  
+  if (! isIdentical) {
+    
+    cat("str(x):\n"); str(x)
+    cat("str(y):\n"); str(y)
+  }
+  
+  isIdentical
+}
+
+# T E S T ----------------------------------------------------------------------
+test_that("merging the unmerged results in the original data frame", {
+  
+  z <- data.frame(
+    insp = c(1, 1, 2, 3, 4, 5),
+    pipe = c(1, 1, 1, 2, 2, 3),
+    p1 = c("a", "a", "a", "b", "b", "c"),
+    p2 = c("A", "A", "A", "B", "B", "c"),
+    i1 = c(100, 150, 200, 100, 200, 100)
+  )
+  
+  expect_true(checkResult(z, unmerge(z, "insp")))
+  expect_true(checkResult(z, unmerge(z, "i1")))
+  expect_true(checkResult(z, unmerge(z, "pipe")))
+  expect_true(checkResult(z, unmerge(z, "p1")))
+  expect_true(checkResult(z, unmerge(z, "p2")))
+  expect_true(checkResult(z, unmerge(z, c("p1", "p2"))))
+})
