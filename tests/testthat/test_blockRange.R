92e9c5e83f33ccdc39328b5cb1fdf7e59d4e02ce
hsonne <hauke.sonnenberg@gmx.de>
2018-05-19 20:01:50 +0200

    add tests of block range functions

---
+++ tests/testthat/test_blockRange.R
@@ -0,0 +1,63 @@
+test_that("extractRowRanges() works", {
+
+  dataFrame <- as.data.frame(stringsAsFactors = FALSE, matrix(
+    ncol = 2, byrow = TRUE, c(
+      "Date", "Value",
+      "1.1.", "1",
+      "2.1.", "2",
+      "", "",
+      "Date", "Value",
+      "3.1.", "3",
+      "4.1.", "4"
+    )
+  ))
+  
+  y <- extractRowRanges(
+    dataFrame, columnName = "V1", pattern = "Date", stopOffset = 2
+  )
+  
+  expected <- list(
+    data.frame(
+      Date = c("1.1.", "2.1."),
+      Value = c("1", "2"),
+      stringsAsFactors = FALSE
+    ),
+    data.frame(
+      Date = c("3.1.", "4.1."),
+      Value = c("3", "4"),
+      stringsAsFactors = FALSE
+    )
+  )
+  
+  expect_identical(y, expected)
+})
+
+test_that("startsToRanges() works", {
+  
+  starts <- c(1, 10, 20, 35)
+  
+  expect_identical(startsToRanges(starts, lastStop = 50), data.frame(
+    from = c(2, 11, 21, 36),
+    to = c(9, 19, 34, 50)
+  ))
+  
+  expect_identical(
+    startsToRanges(starts, lastStop = 55, startOffset = 2, stopOffset = 2),
+    data.frame(
+      from = c(3, 12, 22, 37),
+      to = c(8, 18, 33, 55)
+    )
+  )
+})
+
+test_that("startsToEnds() works", {
+  
+  starts <- c(1, 10, 20, 35)
+  
+  expect_identical(startsToEnds(starts, lastStop = 50), c(9, 19, 34, 50))
+  
+  expect_identical(
+    startsToEnds(starts, lastStop = 50, stopOffset = 2), 
+    c(8, 18, 33, 50)
+  )
+})
