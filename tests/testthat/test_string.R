419a7fb5cbaf29b41e04e624cfd94eb55934e064
hsonne <hauke.sonnenberg@gmx.de>
2018-05-22 00:33:04 +0200

    Add more tests for string functions

--- tests/testthat/test_string.R
+++ tests/testthat/test_string.R
@@ -94,3 +94,110 @@ test_that("collapsed() works", {
   expect_equal(collapsed(c("a", "b")), "a b")
   expect_equal(collapsed(1:5, "@"), "1@2@3@4@5")
 })
+
+test_that("hsQuoteChr() works", {
+
+  expect_identical(hsQuoteChr("a"), "'a'")
+  expect_identical(hsQuoteChr("He says: \"Hello\""), "'He says: \"Hello\"'")
+  expect_identical(
+    hsQuoteChr("He says: \"Hello\"", '"'), "\"He says: \"\"Hello\"\"\""
+  )
+})
+
+test_that("multiSubstitute() works", {
+  
+  expect_identical(multiSubstitute("abc", list(a = "b", b = "c")), "ccc")
+  expect_identical(multiSubstitute("abc", list(a = "b", "^b" = "c")), "cbc")
+})
+
+test_that("hsTrim() works", {
+
+  expect_identical(hsTrim("  a "), "a")
+  expect_identical(hsTrim("  a    b "), "a b")
+  expect_identical(hsTrim(" \ta \t b \t"), "a b")
+})
+
+test_that("removeSpaces() works", {
+
+  expect_identical(removeSpaces(" a b c "), "abc")
+  expect_identical(removeSpaces(" a  b  c "), "abc")
+})
+
+test_that("hsSubstSpecChars() works", {
+  
+  expect_identical(hsSubstSpecChars("% ?"), "proz")
+}) 
+
+test_that("stringToExpression() works", {
+
+  expect_true(is.expression(stringToExpression("a + b")))
+}) 
+
+test_that("stringContains() works", {
+
+  expect_true(all(stringContains(c("abc", "Kabeljau", "Arabella"), "ab")))
+  
+  expect_identical(stringContains(c("abc", "Kabeljau", "Arabella"), "abc"), 
+                   c(TRUE, FALSE, FALSE))
+})
+
+test_that("stringStartsWith() works", {
+
+  expect_identical(
+    stringStartsWith(c("abc", "Kabeljau", "Arabella"), "ab"),
+    c(TRUE, FALSE, FALSE)
+  )
+  
+  expect_identical(
+    stringStartsWith(c("abc", "Kabeljau", "Arabella"), "A"),
+    c(FALSE, FALSE, TRUE)
+  )
+})
+
+test_that("stringEndsWith() works", {
+
+  expect_identical(
+    stringEndsWith(c("abc", "Kabeljau", "Arabella"), "a"),
+    c(FALSE, FALSE, TRUE)
+  )
+  
+  expect_identical(
+    stringEndsWith(c("abc", "Kabeljau", "Arabella"), "jau"),
+    c(FALSE, TRUE, FALSE)
+  )
+})
+
+test_that("extractSubstring() works", {
+  
+  pattern <- "([^ ]+), ([0-9]+) of ([^ ]+)"
+  
+  datestring <- "Thursday, 8 of December"
+  expect_identical(extractSubstring(pattern, datestring, 1), "Thursday")
+  expect_identical(extractSubstring(pattern, datestring, 2), "8")
+  expect_identical(extractSubstring(pattern, datestring, 3), "December")
+  
+  datestrings <- c("Thursday, 8 of December", "Tuesday, 14 of January")
+  expect_identical(
+    extractSubstring(pattern, datestrings, 1), c("Thursday", "Tuesday")
+  )
+  expect_identical(
+    extractSubstring(pattern, datestrings, 2), c("8", "14")
+  )
+  expect_identical(
+    extractSubstring(pattern, datestrings, 3), c("December", "January")
+  )
+  
+  y1 <- extractSubstring(pattern, datestrings, 1:3)
+  
+  y2 <- extractSubstring(
+    pattern, datestrings, index = c(weekday = 1, 2, month = 3)
+  )
+
+  expect_true(is.data.frame(y1))
+  expect_true(is.data.frame(y2))
+
+  expect_identical(dim(y1), c(2L, 3L))
+  expect_identical(dim(y2), c(2L, 3L))
+  
+  expect_identical(names(y2)[c(1, 3)], c("weekday", "month"))
+})
