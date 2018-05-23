c8706701d1a2fb91d06917e1ec5d12830095822b
hsonne <hauke.sonnenberg@gmx.de>
2018-05-23 09:55:25 +0200

    Handle case of unnamed list in test of mergeAll()

--- tests/testthat/test_merge.R
+++ tests/testthat/test_merge.R
@@ -47,19 +47,23 @@ test_that("mergeAll() and safeMerge() work", {
   
   expect_identical(y, merge(peter, paul, by = "fruit"))
   
-  x <- list(peter = peter, paul = paul, mary = mary)
+  x_1 <- list(peter = peter, paul = paul, mary = mary)
+  x_2 <- list(peter, paul, mary)
   
-  expect_output(y_1 <- mergeAll(x, by = "fruit"))
-  expect_output(y_2 <- mergeAll(x, by = "fruit", all = TRUE))
+  expect_output(y_1a <- mergeAll(x_1, by = "fruit"))
+  expect_output(y_1b <- mergeAll(x_1, by = "fruit", all = TRUE))
+  expect_output(y_2b <- mergeAll(x_2, by = "fruit", all = TRUE))
   
-  expect_silent(mergeAll(x, by = "fruit", dbg = FALSE))
+  expect_silent(mergeAll(x_1, by = "fruit", dbg = FALSE))
 
-  expect_identical(nrow(y_1), 1L)
+  expect_identical(nrow(y_1a), 1L)
   
   expect_identical(
-    sort(y_2$fruit), 
+    sort(y_1b$fruit), 
     sort(unique(c(peter$fruit, paul$fruit, mary$fruit)))
   )
+  
+  expect_identical(unname(y_1b), unname(y_2b))
 })
 
 test_that("mergeNamedArrays works", {
