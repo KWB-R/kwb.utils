fcd7e4b0a2eb817ae65fbe672431c3cbf06b6592
hsonne <hauke.sonnenberg@gmx.de>
2018-05-23 11:20:39 +0200

    Use helper function in test of convertCsvFile()

--- tests/testthat/test_convertCsvFile.R
+++ tests/testthat/test_convertCsvFile.R
@@ -1,32 +1,38 @@
 test_that("convertCsvFile() works", {
   
-  csv_in <- tempfile(fileext = ".csv")
+  csv_in_1 <- tempfile(fileext = ".csv")
+  csv_in_2 <- tempfile(fileext = ".csv")
+  csv_in_3 <- tempfile(fileext = ".csv")
   
-  write.table(iris, csv_in, row.names = FALSE)
+  write.table(iris, csv_in_1, row.names = FALSE)
+  write.table(iris, csv_in_2, row.names = TRUE)
+  write.table(iris, csv_in_3, col.names = FALSE)
+  
+  check_result <- function(csv_in, args_convert, args_read) {
+  
+    csv_out <- do.call(convertCsvFile, c(list(csv_in), args_convert))
+    
+    on.exit(unlink(csv_out))
+  
+    # Test that existing file will not be overwritten
+    expect_error(convertCsvFile(csv_in))
+    
+    expect_true(is.character(csv_out))
+    
+    expect_length(csv_out, 1)
+    
+    expect_identical(iris, do.call(read.table, c(list(csv_out), args_read)))
+  }
 
-  csv_out <- convertCsvFile(csv_in, sep_out = ";")
+  check_result(
+    csv_in = csv_in_1, 
+    args_convert = list(sep_out = ";"), 
+    args_read = list(sep = ";", header = TRUE)
+  )
   
-  # Test that existing file will not be overwritten
-  expect_error(convertCsvFile(csv_in))
-  
-  on.exit(unlink(csv_out))
-  
-  expect_true(is.character(csv_out))
-  
-  expect_length(csv_out, 1)
-  
-  iris_reread <- read.table(csv_out, sep = ";", header = TRUE)
-  
-  expect_identical(iris, iris_reread)
-
-  unlink(csv_out)
-  
-  csv_out <- convertCsvFile(csv_in, sep_out = ";", dec_out = ",")
-  
-  expect_true(is.character(csv_out))
-  expect_length(csv_out, 1)
-
-  iris_reread <- read.table(csv_out, sep = ";", dec = ",", header = TRUE)
-  
-  expect_identical(iris, iris_reread)
+  check_result(
+    csv_in = csv_in_1, 
+    args_convert = list(sep_out = ";", dec_out = ","),
+    args_read = list(sep = ";", dec = ",", header = TRUE)
+  )
 })
