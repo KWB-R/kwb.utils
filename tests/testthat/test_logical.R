671bc1727bdfb74a93426d8a4af789fe59986de1
Hauke Sonnenberg <hauke.sonnenberg@kompetenz-wasser.de>
2018-05-24 11:51:32 +0200

    Move one test and add further logical tests

--- tests/testthat/test_logical.R
+++ tests/testthat/test_logical.R
@@ -1,13 +1,67 @@
-test_that("almostEqual works", {
+test_that("almostEqual() works", {
+  
   expect_error(almostEqual(1, 1:2))
   expect_error(almostEqual(1:2, 3))
+  
   expect_false(almostEqual(1, 2))
   expect_true(almostEqual(1, 1))
+  
   expect_false(almostEqual(1, 1.1, 0.1))
   expect_true(almostEqual(1, 1.09, 0.1))
 })
 
-test_that("allAreEqual works", {
-  expect_true(allAreEqual(c(1, 1, 1)))
-  expect_false(allAreEqual(c(1, 1, 2)))
+test_that("allAreEqual() works", {
+  
+  x_1 <- c(1, 1, 1)
+  x_2 <- c(1, 1, 2)
+  
+  expect_true(allAreEqual(x_1))
+  expect_false(allAreEqual(x_2))
+  
+  expect_true(allAreEqual(x_1, method = 2))
+  expect_false(allAreEqual(x_2, method = 2))
+})
+
+test_that("isNaInAllColumns() and isNaInAllRows() work", {
+  
+  x_1 <- data.frame(a = 1:2, b = NA)
+  x_2 <- rbind(x_1, data.frame(a = NA, b = NA))
+  
+  expect_identical(isNaInAllColumns(x_1), c(FALSE, FALSE))
+  expect_identical(isNaInAllColumns(x_2), c(FALSE, FALSE, TRUE))
+  
+  expect_identical(isNaInAllRows(x_1), c(FALSE, TRUE))
+  expect_identical(isNaInAllRows(x_2), c(FALSE, TRUE))
+  
+  expect_error(isNaInAllColumns(1))
+  expect_error(isNaInAllRows(1))
+})
+
+test_that("allAreIdentical() works", {
+  
+  expect_error(allAreIdentical(1))
+  expect_error(allAreIdentical(list()))
+  
+  expect_message(allAreIdentical(list(1)))
+  
+  expect_true(allAreIdentical(list(1, 1)))
+  expect_false(allAreIdentical(list(1, 2)))
+})
+
+test_that("matchesCriteria() works", {
+  
+  Data <- data.frame(A = c("x", "y", "z"), B = c( 1,   2,   3))
+  
+  criteria1 <- c("A %in% c('y', 'z')", "B %in% 1:3")
+  criteria2 <- c("AB %in% c('y', 'z')", "B <- 1")
+  
+  expect_equal(matchesCriteria(Data, criteria1), c(F, T, T))
+  expect_equal(matchesCriteria(Data, criteria1[1]), c(F, T, T))
+  expect_equal(matchesCriteria(Data, criteria1[2]), c(T, T, T))
+  
+  expect_error(matchesCriteria(Data, criteria2))
+  expect_error(matchesCriteria(Data, criteria2[1]))
+  expect_error(matchesCriteria(Data, criteria2[2]))
+  
+  expect_true(all(matchesCriteria(Data)))
 })
--- tests/testthat/test_roundColumns.R
+++ tests/testthat/test_roundColumns.R
@@ -12,21 +12,3 @@ test_that("roundColumns() works", {
   expect_equal(x2$a, round(x$a, 1))
   expect_equal(x2$c, round(x$c, -2))
 }) 
-
-test_that("matchesCriteria() works", {
-  
-  Data <- data.frame(A = c("x", "y", "z"), B = c( 1,   2,   3))
-  
-  criteria1 <- c("A %in% c('y', 'z')", "B %in% 1:3")
-  criteria2 <- c("AB %in% c('y', 'z')", "B <- 1")
-  
-  expect_equal(matchesCriteria(Data, criteria1), c(F, T, T))
-  expect_equal(matchesCriteria(Data, criteria1[1]), c(F, T, T))
-  expect_equal(matchesCriteria(Data, criteria1[2]), c(T, T, T))
-  
-  expect_error(matchesCriteria(Data, criteria2))
-  expect_error(matchesCriteria(Data, criteria2[1]))
-  expect_error(matchesCriteria(Data, criteria2[2]))
-  
-  expect_true(all(matchesCriteria(Data)))
-})
