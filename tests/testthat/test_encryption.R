1705b23dc2072655dfdbd4664a29e31314fd8c05
hsonne <hauke.sonnenberg@gmx.de>
2018-05-25 02:18:21 +0200

    Add test for getPassword()

--- tests/testthat/test_encryption.R
+++ tests/testthat/test_encryption.R
@@ -24,7 +24,7 @@ test_that(".checkNamespace() works", {
 
 test_that("createPasswordFile() and getPassword() work", {
 
-  key_file <- tempfile()
+  key_file <- tempfile(fileext = ".txt")
   
   password_file <- tempfile()
   
@@ -35,4 +35,8 @@ test_that("createPasswordFile() and getPassword() work", {
   createPasswordFile("hauke", key_file, password_file, password = "abc")
   
   expect_identical(getPassword(password_file, key_file), "abc")
+  
+  expect_error(getPassword("no_such_file"))
+  
+  expect_output(.askForPassword("hauke"))
 })
