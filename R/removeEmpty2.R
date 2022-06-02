9dffb2275024ef7a7a4cc2cbe52ea181f36bf48f
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-06-02 23:18:15 +0200

    Add documentation for and export removeEmpty2()

--- R/removeEmpty2.R
+++ R/removeEmpty2.R
@@ -1,3 +1,12 @@
+#' Remove Empty Elements from Vector of Character
+#' 
+#' @param x vector of character
+#' @param dbg if \code{TRUE} (the default is \code{FALSE}) a message about the 
+#'   number of removed elements is printed
+#' @export
+#' @examples
+#' removeEmpty2(c("a", "", "c"))
+#' removeEmpty2(c("a", "", "c", "", "e"), dbg = TRUE)
 removeEmpty2 <- function(x, dbg = FALSE)
 {
   isEmpty <- (x == "")
