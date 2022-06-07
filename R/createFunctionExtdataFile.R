5280df3c2d32ad23ecf990b7c177a1b1cc3cc4a9
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-06-07 08:43:49 +0200

    Add extdataFile()

--- R/createFunctionExtdataFile.R
+++ R/createFunctionExtdataFile.R
@@ -12,3 +12,14 @@ createFunctionExtdataFile <- function(package)
 {
   function(...) system.file("extdata", ..., package = package)
 }
+
+#' Path to File in Installed Package
+#' 
+#' @param \dots parts of the file path to be passed to \code{\link{system.file}}
+#' @return path to file in the package installation folder in the R library
+#'   or "" if the path does not exist
+#' @export
+#' @examples 
+#' # List the files provided in the "extdata" folder of kwb.utils
+#' dir(extdataFile())
+extdataFile <- createFunctionExtdataFile("kwb.utils")
