356862f3ec0b43d7c976adbb4ba523a0886d8074
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-06-09 13:24:13 +0200

    Add arg "must_exist" to extdataFile()
    and to the functions that are returned by
    createFunctionExtdataFile()

--- R/createFunctionExtdataFile.R
+++ R/createFunctionExtdataFile.R
@@ -10,12 +10,23 @@
 #' dir(extdataFile())
 createFunctionExtdataFile <- function(package)
 {
-  function(...) system.file("extdata", ..., package = package)
+  function(..., must_exist = FALSE) {
+    
+    extdata_dir <- system.file("extdata", package = package)
+
+    if (must_exist) {
+      safePath(extdata_dir, ...)
+    } else {
+      do.call(file.path, list(extdata_dir, ...))
+    }
+  }
 }
 
 #' Path to File in Installed Package
 #' 
 #' @param \dots parts of the file path to be passed to \code{\link{system.file}}
+#' @param must_exist if \code{TRUE} and the specified file does not exist, the 
+#'   program stops with an error message
 #' @return path to file in the package installation folder in the R library
 #'   or "" if the path does not exist
 #' @export
