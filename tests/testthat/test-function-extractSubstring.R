d6a4fd8f5983b6404108c5fe6863957714b3eab1
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-08-05 08:52:51 +0200

    Clean and extend tests for extractSubstring()

--- tests/testthat/test-function-extractSubstring.R
+++ tests/testthat/test-function-extractSubstring.R
@@ -1,34 +1,38 @@
 test_that("extractSubstring() works", {
-  
+
+  f <- kwb.utils::extractSubstring  
   pattern <- "([^ ]+), ([0-9]+) of ([^ ]+)"
   
-  datestring <- "Thursday, 8 of December"
-  expect_identical(extractSubstring(pattern, datestring, 1), "Thursday")
-  expect_identical(extractSubstring(pattern, datestring, 2), "8")
-  expect_identical(extractSubstring(pattern, datestring, 3), "December")
+  x <- "Thursday, 8 of December"
+  expect_identical(f(pattern, x, 1L), "Thursday")
+  expect_identical(f(pattern, x, 2L), "8")
+  expect_identical(f(pattern, x, 3L), "December")
   
-  datestrings <- c("Thursday, 8 of December", "Tuesday, 14 of January")
-  expect_identical(
-    extractSubstring(pattern, datestrings, 1), c("Thursday", "Tuesday")
-  )
-  expect_identical(
-    extractSubstring(pattern, datestrings, 2), c("8", "14")
-  )
-  expect_identical(
-    extractSubstring(pattern, datestrings, 3), c("December", "January")
+  x <- c(
+    "Thursday, 8 of December", 
+    "Tuesday, 14 of January"
   )
   
-  y1 <- extractSubstring(pattern, datestrings, 1:3)
+  expect_identical(f(pattern, x, 1), c("Thursday", "Tuesday"))
+  expect_identical(f(pattern, x, 2), c("8", "14"))
+  expect_identical(f(pattern, x, 3), c("December", "January"))
   
-  y2 <- extractSubstring(
-    pattern, datestrings, index = c(weekday = 1, 2, month = 3)
-  )
+  y1 <- f(pattern, x, 1:3)
+  y2 <- f(pattern, x, index = c(weekday = 1, 2, month = 3))
+  y3 <- f(pattern, x, 1:3, stringsAsFactors = TRUE)
 
   expect_true(is.data.frame(y1))
   expect_true(is.data.frame(y2))
 
+  check_chr <- function(y) expect_true(all(sapply(y, is.character)))
+  check_chr(y1)
+  check_chr(y2)
+  
+  expect_true(all(sapply(y3, is.factor)))
+  
   expect_identical(dim(y1), c(2L, 3L))
   expect_identical(dim(y2), c(2L, 3L))
   
   expect_identical(names(y2)[c(1, 3)], c("weekday", "month"))
+  
 })
